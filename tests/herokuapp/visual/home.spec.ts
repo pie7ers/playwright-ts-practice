cf09df0bae4424f3009ccf5382b47e00ec3d2ef9
Sebsatian Iguavita Roa <sebastianiguavitaroa@outlook.com>
2025-12-08 10:52:14 -0500

    feat: add visual tests for home and firefox was added to the projects

---
+++ tests/herokuapp/visual/home.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test'
+import HomePage from '../../../pages/herokuapp/HomePage'
+import { CONSTS } from '../../../utils/consts'
+
+test.describe('Herokuapp Home Page', () => {
+  let home: HomePage
+  let evidencePath: string
+
+  test.beforeEach(async ({ page }) => {
+    home = new HomePage(page)
+    evidencePath = `${home.mainPath}/home`
+    await home.goto(CONSTS.HEROKU_BASE_URL)
+  })
+
+  test('should display the correct heading', async () => {
+    const headingText = await home.getHeadingText(home.headTitle)
+    await home.visualTest('home-1.png')
+    await home.scrollToTheEndOfThePage()
+    await home.visualTest('home-2-footer.png')
+  })
+
+  test('should navigate to A/B Testing page when link is clicked', async () => {
+    await home.clickExampleLink('A/B Testing')
+    await home.visualTest('a-b-test-control.png')
+  })
+
+  test('should navigate to Frames page when link is clicked', async () => {
+    await home.clickExampleLink('Frames')
+    await home.visualTest('frames.png')
+  })
+})
